a3ccbbed477de5f40983bf391ee3afdcd5883ab0
Park June Chul <pjc0247@naver.com>
2020-08-10 18:07:54 +0900

    Update useService.js

--- src/useService.js
+++ src/useService.js
@@ -1,20 +1,26 @@
 const services = {};
+let currentSubscription = null;
 const getService = (service) => {
   const name = service.name;
   return services[name];
 };
-const createService = (service, render) => {
+const createService = (service) => {
   const s = new service();
   const p = new Proxy(s, {
     get: (target, prop) => {
       // TODO: build read-map
+      if (prop !== '__subscribers') {
+        currentSubscription.props[prop] = true;
+      }
       return target[prop];
     },
     set: (target, prop, newValue) => {
       target[prop] = newValue;
       if (prop !== '__subscribers') {
-        for (const subscriber of p.__subscribers)
-          subscriber();
+        for (const subscriber of p.__subscribers) {
+          if (subscriber.props[prop])
+            subscriber.render();
+        }
       }
       return newValue;
     },
@@ -30,21 +36,27 @@ const useRerender = () => {
 const useComponentService = (service) => {
   const render = useRerender();
   const instance = React.useMemo(() => {
-    return createService(service, render);
+    return createService(service);
   }, [service]);
   return instance;
 };
 const useService = (service) => {
   const render = useRerender();
+  const subscription = React.useRef({
+    render,
+    props: {},
+  });
   let s = services[service.name];
   if (!s) {
-    s = createService(service, render);
+    s = createService(service);
     services[service.name] = s;
   }
 
   React.useEffect(() => {
-    s.__subscribers.push(render);
+    s.__subscribers.push(subscription.current);
   }, [s]);
   
+  currentSubscription = subscription.current;
+  currentSubscription.props = {};
   return s;
 };
